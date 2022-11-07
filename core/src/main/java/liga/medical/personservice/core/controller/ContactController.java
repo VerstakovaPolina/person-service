a0611056408cd45b0e9148e2ec961cc170bdc5bd
VerstakovaPolina <114863246+VerstakovaPolina@users.noreply.github.com>
2022-11-07 14:51:15 +0300

    hometask-9

--- core/src/main/java/liga/medical/personservice/core/controller/ContactController.java
+++ core/src/main/java/liga/medical/personservice/core/controller/ContactController.java
@@ -6,7 +6,12 @@ import liga.medical.personservice.core.model.Contact;
 import liga.medical.personservice.core.service.ContactService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 
 @RestController
